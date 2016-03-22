6a89bc961de2d8a4f2d5af176eb80bcd7871c5f1
samyra <samyralara@outlook.com>
2016-03-22 19:58:40 -0300

    Alteracao no BD: inclusao do auto-increment nos id´s das tabelas

--- gravadora.sql
+++ gravadora.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.3.1
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: 11-Mar-2016 às 11:01
--- Versão do servidor: 5.5.47-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.14
+-- Host: 127.0.0.1
+-- Generation Time: 22-Mar-2016 às 23:52
+-- Versão do servidor: 5.6.17
+-- PHP Version: 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `gravadora`
@@ -26,10 +26,11 @@ SET time_zone = "+00:00";
 -- Estrutura da tabela `cantor`
 --
 
-CREATE TABLE `cantor` (
-  `codigo` int(11) NOT NULL,
-  `nome` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `cantor` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -37,12 +38,14 @@ CREATE TABLE `cantor` (
 -- Estrutura da tabela `cd`
 --
 
-CREATE TABLE `cd` (
-  `cod_cd` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `cd` (
+  `cod_cd` int(11) NOT NULL AUTO_INCREMENT,
   `titulo` varchar(100) NOT NULL,
   `data_lancamento` date NOT NULL,
-  `cantor_fk` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `cantor_fk` int(11) NOT NULL,
+  PRIMARY KEY (`cod_cd`),
+  KEY `cantor_fk` (`cantor_fk`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -50,55 +53,14 @@ CREATE TABLE `cd` (
 -- Estrutura da tabela `usuario`
 --
 
-CREATE TABLE `usuario` (
-  `cod_user` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `cod_user` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(100) NOT NULL,
   `nome` varchar(100) NOT NULL,
-  `senha` varchar(256) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `senha` varchar(256) NOT NULL,
+  PRIMARY KEY (`cod_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `cantor`
---
-ALTER TABLE `cantor`
-  ADD PRIMARY KEY (`codigo`);
-
---
--- Indexes for table `cd`
---
-ALTER TABLE `cd`
-  ADD PRIMARY KEY (`cod_cd`),
-  ADD KEY `cantor_fk` (`cantor_fk`);
-
---
--- Indexes for table `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`cod_user`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `cantor`
---
-ALTER TABLE `cantor`
-  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `cd`
---
-ALTER TABLE `cd`
-  MODIFY `cod_cd` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `cod_user` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
