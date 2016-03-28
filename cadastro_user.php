0af3c54c9ee8e87535f4254142debe8b365fdcd1
sergiocampos <sergio.campos@dce.ufpb.br>
2016-03-28 19:59:45 -0300

    cadastrar usuario rodando

--- cadastro_user.php
+++ cadastro_user.php
@@ -9,27 +9,16 @@
 <body>
 	<?php 
 	try {
-		$con = new PDO("mysql:host=localhost;dbname=gravadora","root","tguide");
+		$con = new PDO("mysql:host=localhost;dbname=gravadora","root","");
 		echo "conectado!";
 		$login = $_POST['login'];
 		$nome = $_POST['nomeUser'];
 		$senha = $_POST['senha'];
 		$senha_encrypt = md5($senha);
-		//echo "$senha_encrypt";
-		$sql = "select * from usuario";
-		foreach ($con->query($sql) as $key) {
-			if ($login === $key['login']) {			
-			//$login_encontrado = $key['login'];
-			echo "É igual";
-			} else {
-				$count = $con->exec("insert into usuario (login, nome, senha) values 
-					('$login', '$nome', '$senha_encrypt')");
-					echo "$count";
-			}
-
-		}
-
-		
+		$count = $con->exec("insert into usuario (login, nome, senha) values 
+				('$login', '$nome', '$senha_encrypt')");
+				echo "$count";
+					
 	$con = null;	
 	} catch (Exception $e) {
 		echo $e->getMessage();
@@ -40,7 +29,8 @@
 	
 	<form action="" method="POST">
 		Cadastro de Usuário <br>
-		Login:<input type="text" name="login"><br>
+		Login:<input type="text" name="login">
+		<input type="button" name="verificar" value="Verificar"><br>
 		Nome:<input type="text" name="nomeUser"><br>
 		Senha:<input type="password" name="senha"><br>
 		<input type="submit" value="Cadastrar">
